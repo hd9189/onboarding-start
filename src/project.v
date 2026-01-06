f6c37ba210ef089dcc8a594032b387bc31832b53
Hugh Ding <hzfmd2006@gmail.com>
2026-01-06 16:11:15 -0500

    so stupid

--- src/project.v
+++ src/project.v
@@ -34,18 +34,6 @@ module tt_um_uwasic_onboarding_Hugh_Ding (
   wire [7:0] en_reg_pwm_15_8;
   wire [7:0] pwm_duty_cycle;
 
-  // Instantiate the PWM module
-  pwm_peripheral pwm_peripheral_inst (
-    .clk(clk),
-    .rst_n(rst_n),
-    .en_reg_out_7_0(en_reg_out_7_0),
-    .en_reg_out_15_8(en_reg_out_15_8),
-    .en_reg_pwm_7_0(en_reg_pwm_7_0),
-    .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwm_duty_cycle),
-    .out({uio_out, uo_out})
-  );
-  // Add uio_in and ui_in[7:3] to the list of unused signals:
-  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+  
 
 endmodule
