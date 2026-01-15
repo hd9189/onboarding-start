d54b9f5e45c85655cecb3bee335c772ac02f5542
Hugh Ding <hzfmd2006@gmail.com>
2026-01-15 14:52:08 -0500

    fixed output

--- src/project.v
+++ src/project.v
@@ -18,8 +18,8 @@ module tt_um_uwasic_onboarding_Hugh_Ding (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+  assign uo_out  = en_reg_out_7_0;  // Connect to SPI output register
+  assign uio_out = en_reg_out_15_8;  // Connect to second SPI output register
   // assign uio_oe  = 0;
   assign uio_oe = 8'hFF; // Set all IOs to output
   
