5e2d0d46c4e7df7cb5a82f4c10017f047ed499dd
Hugh Ding <hzfmd2006@gmail.com>
2026-01-15 14:44:02 -0500

    missing semicolon

--- src/project.v
+++ src/project.v
@@ -43,7 +43,7 @@ module tt_um_uwasic_onboarding_Hugh_Ding (
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
     .pwm_duty_cycle(pwm_duty_cycle),
     .out({uio_out, uo_out}
-)
+);
 
 spi_peripheral spi_peripheral_instance (
     .clk(clk),
