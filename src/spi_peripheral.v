c5892b9600e6c75c3d11db8fa79eb87c4016e6a9
Hugh Ding <hzfmd2006@gmail.com>
2026-01-15 16:47:32 -0500

    test_compille

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -46,18 +46,19 @@ module spi_peripheral (
             data <= 16'b0;
             current_bit_shift <= 5'b0;
             write_done <= 1'b0;
-        end else if (cs_sync == 2'b00 && SCLK_sync == 2'b01 && current_bit_shift != 5'b10000) begin
+        end else if (cs_sync == 2'b00 && SCLK_sync == 2'b01 && current_bit_shift != 5'b10000) begin // sclk rising edge
             data[current_bit_shift] <= COPI_sync[1];
             current_bit_shift <= current_bit_shift + 1;
         end
 
-        if (current_bit_shift == 5'b10000 && data[15] == 1'b1 && write_done == 1'b0) begin // finished shifting and write, write only once
+        if (current_bit_shift == 5'b10000 && write_done == 1'b0) begin // finished shifting and write, write only once
             case (data[14:8])
                 7'h00 : en_reg_out_7_0 <= data[7:0];
                 7'h01 : en_reg_out_15_8 <= data[7:0];
                 7'h02 : en_reg_pwm_7_0 <= data[7:0];
                 7'h03 : en_reg_pwm_15_8 <= data[7:0];
                 7'h04 : pwm_duty_cycle <= data[7:0];
+                default: ;
             endcase
             write_done <= 1'b1;
         end
