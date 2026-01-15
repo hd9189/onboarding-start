c8f86a5dbba343017d1a0e7624014dfbfe29fc73
Hugh Ding <hzfmd2006@gmail.com>
2026-01-15 17:07:50 -0500

    trying to fix gds

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -41,25 +41,25 @@ module spi_peripheral (
             SCLK_sync <= {SCLK_sync[0], sclk};
             COPI_sync <= {COPI_sync[0], COPI};
             cs_sync <= {cs_sync[0], cs};
-        end
 
-        if (cs_sync == 2'b10) begin // cs falling edge, begin data capture
-            data <= 16'b0;
-            current_bit_shift <= 5'b0;
-        end else if (cs_sync == 2'b00 && SCLK_sync == 2'b01 && current_bit_shift < 5'd16) begin // sclk rising edge
-            data[15 - current_bit_shift] <= COPI_sync[1];
-            current_bit_shift <= current_bit_shift + 1;
-        end
-        if (cs_sync == 2'b01) begin  // CS rising edge
-            if (current_bit_shift == 5'b10000 && data[15] == 1) begin // finished shifting and write, write only once
-                case (data[14:8])
-                    7'h00 : en_reg_out_7_0 <= data[7:0];
-                    7'h01 : en_reg_out_15_8 <= data[7:0];
-                    7'h02 : en_reg_pwm_7_0 <= data[7:0];
-                    7'h03 : en_reg_pwm_15_8 <= data[7:0];
-                    7'h04 : pwm_duty_cycle <= data[7:0];
-                    default: ;
-                endcase
+            if (cs_sync == 2'b10) begin // cs falling edge, begin data capture
+                data <= 16'b0;
+                current_bit_shift <= 5'b0;
+            end else if (cs_sync == 2'b00 && SCLK_sync == 2'b01 && current_bit_shift < 5'd16) begin // sclk rising edge
+                data[15 - current_bit_shift] <= COPI_sync[1];
+                current_bit_shift <= current_bit_shift + 1;
+            end
+            if (cs_sync == 2'b01) begin  // CS rising edge
+                if (current_bit_shift == 5'b10000 && data[15] == 1) begin // finished shifting and write, write only once
+                    case (data[14:8])
+                        7'h00 : en_reg_out_7_0 <= data[7:0];
+                        7'h01 : en_reg_out_15_8 <= data[7:0];
+                        7'h02 : en_reg_pwm_7_0 <= data[7:0];
+                        7'h03 : en_reg_pwm_15_8 <= data[7:0];
+                        7'h04 : pwm_duty_cycle <= data[7:0];
+                        default: ;
+                    endcase
+                end
             end
         end
     end
